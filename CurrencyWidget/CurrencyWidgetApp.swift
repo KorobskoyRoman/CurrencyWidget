2917facbb3c4b8e64aa29c326c22b7cf94a02e8d
KorobskoyRoman <shopone023@gmail.com>
2023-07-19 18:32:17 +0300

    Initial Commit

---
+++ CurrencyWidget/CurrencyWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyWidgetApp.swift
+//  CurrencyWidget
+//
+//  Created by Roman Korobskoy on 19.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
