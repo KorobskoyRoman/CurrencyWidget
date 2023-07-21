c9d5dfb44c51dbc485bf259b71418beacc20cc37
KorobskoyRoman <shopone023@gmail.com>
2023-07-21 20:09:58 +0300

    feat: Update currency with currency symbol

--- CurrencyWidget/Core/Models/Currency.swift
+++ CurrencyWidget/Core/Models/Currency.swift
@@ -12,27 +12,28 @@ struct Currency: Codable {
     let success: Bool
     let timestamp: Int
     let base, date: String
-    let rates: Rates
+    let rates: [String: Double]
+
+    func currencySymbol(for code: String) -> String {
+        switch code {
+        case "USD":
+            return " $"
+        case "EUR":
+            return " €"
+        case "AED":
+            return " د.إ"
+        case "RUB":
+            return " ₽"
+        default:
+            return code
+        }
+    }
 
     static let mockCurrency: Currency = .init(
         success: true,
         timestamp: 1689878463,
-        base: "RUB",
+        base: "EUR",
         date: "2023-07-20",
-        rates: .init(usd: 110.2, eur: 123.43, rub: 1, aed: 55.55)
+        rates: ["USD": 1.234341414]
     )
-}
-
-// MARK: - Rates
-struct Rates: Codable {
-    let usd: Double
-    let eur: Double
-    let rub, aed: Double
-
-    enum CodingKeys: String, CodingKey {
-        case usd = "USD"
-        case eur = "EUR"
-        case rub = "RUB"
-        case aed = "AED"
-    }
 }
